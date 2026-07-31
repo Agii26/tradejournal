03f99ffacac1140af4f8706a8444e4478c307e61
rhey <rheychristian26@gmail.com>
2026-07-31 15:56:09 +0800

    Add username/isPublicProfile/isPrivate migration

---
+++ prisma/migrations/20260731075602_add_public_profile_fields/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[username]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "trades" ADD COLUMN     "isPrivate" BOOLEAN NOT NULL DEFAULT false;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "isPublicProfile" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "username" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
